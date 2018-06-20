f504bc4f3d821851ac131d9da355e6c47a967114
Gabriel Weyer <gabrielweyer@gmail.com>
2018-06-20 18:21:20 +1000

    Dirty first version

---
+++ README.md
@@ -0,0 +1,44 @@
+# Windows Azure Diagnostics to CSV
+
+`.NET Core` [global tool][dotnet-global-tools] to retrieve `MessageEvent`s from a `WADLogsTable` and write them to a `CSV` file.
+
+Only `MessageEvent`s starting with `EventName="MessageEvent" Message="` will be parsed and written to the output file. This is to discard `MessageEvent`s such as this one `EventName="DirectWrite" Message="Getting "TopSecret" from ServiceRuntime: PASS (<your-top-secret-value>)."` (refer to this [GitHub issue][github-issue] for context).
+
+Don't expect too much, the goal is to take **some** of the pain away :trollface:.
+
+## Installation
+
+```posh
+> dotnet tool install -g dotnet-wad-to-csv
+```
+
+## Usage
+
+```posh
+> dotnet wad-to-csv -d <duration> -o <output-file-path>
+```
+
+- `<duration>`: based on the `time designator` of the [ISO 8601 durations][iso-8601-duration]. This duration is then substracted from the current `UTC` time. For example:
+  - `5M`: get all the `MessageEvent`s for the last `5` **minutes**
+  - `1H`: get all the `MessageEvent`s for the last `1` **hour**
+  - You can combine them too if you feel like it, i.e. `2H3M5S` would get all the `MessageEvent`s for the last `7385` **seconds**
+- `<output-file-path>`: where you wish to write the output file, does not need to exist but should be valid. If a file exists with the same name it will be replaced.
+
+The tool will prompt you for a [Shared Access Signature][sas] so that it doesn't get saved to your `CLI` history. I recommend you restrict the `SAS` to:
+
+- `Read` and `List` permissions
+- `Tables` service
+- `Container` and `Object` resource types
+- A short expiration time
+
+## Output file format
+
+```csv
+Generated,Level,Message
+2018-06-18T09:50:28.155T,Information,"Some logging event I wrote"
+```
+
+[iso-8601-duration]: https://en.wikipedia.org/wiki/ISO_8601#Durations
+[dotnet-global-tools]: https://docs.microsoft.com/en-us/dotnet/core/tools/global-tools
+[sas]: https://docs.microsoft.com/en-us/azure/storage/common/storage-dotnet-shared-access-signature-part-1
+[github-issue]: https://github.com/Azure/azure-sdk-for-net/issues/653
---
+++ src/dotnet-wad-to-csv/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DotNet.WadToCsv
+{
+    class Program
+    {
+        [Option(ShortName = "d", LongName = "duration", Description = "ISO 8601 duration")]
+        public string Duration { get; }
+
+        [Required]
+        [Option(ShortName = "o", LongName = "output", Description = "Output file path")]
+        public string OutputFilePath { get; }
+
+        static Task<int> Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
+
+        private async Task OnExecuteAsync()
+        {
+            // TODO: test validity of output file path
+
+            var fullPath = Path.GetFullPath(OutputFilePath);
+
+            // TODO: default duration if missing
+
+            // TODO: cancellation token
+
+            var storageConnectionString = Prompt.GetPassword("SAS", ConsoleColor.White, ConsoleColor.DarkBlue);
+
+            var storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var table =  tableClient.GetTableReference("WADLogsTable");
+
+            if (!await table.ExistsAsync())
+            {
+                throw new InvalidOperationException("The table 'WADLogsTable' does not exist in this storage account.");
+            }
+
+            // TODO: compute timestamp
+
+            const string since = "0636649120905773478";
+
+            var query = new TableQuery<DynamicTableEntity>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.GreaterThan, since))
+                .Select(new[] { "PreciseTimeStamp", "Level", "Message" });
+
+            EntityResolver<WadLogs> resolver = (pk, rk, ts, props, etag) => new WadLogs {
+                Generated = props["PreciseTimeStamp"].DateTime.GetValueOrDefault(),
+                Level = ParseLevel(props["Level"].Int32Value),
+                Message = props["Message"].StringValue,
+            };
+
+            TableContinuationToken continuationToken = null;
+
+            using (var outputFile = File.CreateText(fullPath))
+            {
+                outputFile.WriteLine("Generated,Level,Message");
+
+                do
+                {
+                    var result = await table.ExecuteQuerySegmentedAsync(query, resolver, continuationToken);
+
+                    foreach (var log in result.Results)
+                    {
+                        if (log.Message.Length <= 12 || log.Message[11] != 'M')
+                        {
+                            continue;
+                        }
+
+                        outputFile.WriteLine($"{FormatGenerated(log.Generated)},{log.Level},{FormatMessage(log.Message)}");
+                    }
+
+                    continuationToken = result.ContinuationToken;
+                } while (continuationToken != null);
+            }
+
+            string ParseLevel(int? level)
+            {
+                // TODO: investigate why Warning is 3 and Information is 4
+
+                switch (level)
+                {
+                    case 1:
+                        return "Critical";
+                    case 2:
+                        return "Error";
+                    case 4:
+                        return "Information";
+                    case 3:
+                        return "Warning";
+                    case 5:
+                        return "Verbose";
+                }
+
+                return "Undefined";
+            }
+
+            string FormatMessage(string message)
+            {
+                return message.Substring(33, message.Length - 23 - 33);
+            }
+
+            string FormatGenerated(DateTime generated)
+            {
+                return generated.ToString("yyyy-MM-ddTHH:mm:ss.fffT");
+            }
+        }
+
+        class WadLogs
+        {
+            public DateTime Generated { get; set; }
+            public string Message { get; set; }
+            public string Level { get; set; }
+        }
+    }
+}
